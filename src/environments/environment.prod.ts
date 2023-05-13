b4d209ceadba3e6a95ffe8868cd455ce7c8de727
TonyoCallimoutou <tonyo.callimoutou@hotmail.fr>
2023-05-13 10:38:02 +0200

    fix: env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
   production: true
+  Back_Host: process.env['IP_MYSQL'] as string + ':' + process.env['PORT_BACK'] as string,
+
+  firebaseConfig: {
+    apiKey: process.env['FIREBASE_API_KEY'] as string,
+    authDomain: process.env['FIREBASE_AUTH_DOMAIN'] as string,
+    projectId: process.env['FIREBASE_PROJECT_ID'] as string,
+    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'] as string,
+    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'] as string,
+    appId: process.env['FIREBASE_APP_ID'] as string,
+    measurementId: process.env['FIREBASE_MEASUREMENT_ID'] as string
+  }
 };
