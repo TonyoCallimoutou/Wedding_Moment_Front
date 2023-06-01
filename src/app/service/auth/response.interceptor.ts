1041de01b01e1e2bec08bcf1c71095689a6caa8c
Tonyo Callimoutou <tonyo.callimoutou@hotmail.fr>
2023-06-01 17:56:10 +0200

    fix: token expired

--- src/app/service/auth/response.interceptor.ts
+++ src/app/service/auth/response.interceptor.ts
@@ -1,17 +1,34 @@
 import {Injectable} from "@angular/core";
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import { Observable, tap } from "rxjs";
+import {Observable, take, tap} from "rxjs";
+import {LocalModel} from "../../model/local.model";
+import {UserModelService} from "../../viewModel/user-model.service";
 
 @Injectable()
 export class ResponseInterceptor implements HttpInterceptor {
+
+  constructor(
+    private userModelService: UserModelService,
+    ) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       tap(
         (event: HttpEvent<any>) => {},
         (error) => {
           if (error.status === 403) {
-            console.log('Unauthorized access');
-            console.log(req.url);
+            if (error.error.code === "auth/id-token-expired") {
+              console.log(error.error);
+              console.log("error on : " + req.url);
+
+              this.userModelService.getUserFromDB("0")
+                .pipe(take(1))
+                .subscribe((user: User) => {
+                  localStorage.setItem(LocalModel.TOKEN, '');
+                  localStorage.setItem(LocalModel.USER, JSON.stringify(user));
+                  window.location.reload();
+                });
+            }
           }
         }
       )
