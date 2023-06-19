94a251a473d863c83bcce8be295c0da601d59429
Tonyo Callimoutou <tonyo.callimoutou@hotmail.fr>
2023-06-19 18:54:28 +0200

    feat: home-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import {DashboardComponent} from "./page/home-page/dashboard/dashboard.component
 import {AuthGuard} from './service/auth/auth.guard';
 import {PageNotFoundComponent} from './page/page-not-found/page-not-found.component';
 import {FormulaireInscriptionComponent} from "./page/website/formulaire-inscription/formulaire-inscription.component";
+import {HomeComponent} from "./page/home-page/home/home.component";
 
 const routes: Routes = [
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: '', redirectTo: '/home-page', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+  {path: 'home-page', component: HomeComponent},
   {path: 'inscription', component: FormulaireInscriptionComponent},
 
   // Last Path
