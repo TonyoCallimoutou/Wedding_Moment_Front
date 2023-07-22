d948441e92bb5612f2148ed961b8f44815bd3fb5
Tonyo Callimoutou <tonyo.callimoutou@hotmail.fr>
2023-07-22 15:28:31 +0200

    feat: option posts

--- src/app/shared/component/card-post/card-post.component.ts
+++ src/app/shared/component/card-post/card-post.component.ts
@@ -1,22 +1,78 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, TemplateRef, ViewChild} from '@angular/core';
+import {OptionStringIcon} from "../../../model/option-string-icon.model";
+import {TranslateService} from "@ngx-translate/core";
+import {SnackbarService} from "../../service/snackbar.service";
 
 @Component({
   selector: 'app-card-post',
   templateUrl: './card-post.component.html',
   styleUrls: ['./card-post.component.scss']
 })
-export class CardPostComponent {
+export class CardPostComponent implements OnChanges{
 
   @Input() post! : Post;
   @Input() reactPostId : number[] = [];
   @Input() currentUser! : User;
   @Input() style : string = '';
   @Input() isActivate : boolean = false;
-
   @Output() reaction : EventEmitter<Post> = new EventEmitter<Post>();
   @Output() delete : EventEmitter<Post> = new EventEmitter<Post>();
 
-  constructor() {
+  public dropdownOptions: OptionStringIcon[] = [];
+
+  constructor(
+    private translate: TranslateService,
+    private snackbarService: SnackbarService,
+  ) {
+    console.log(this.post);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['post']) {
+      this.initDropDownOption();
+    }
+  }
+
+
+  /**
+   * Set dropdownOptions
+   */
+  initDropDownOption() {
+
+    let option1 : OptionStringIcon = {
+      optionText: "",
+      icon: "report",
+    }
+    let option2: OptionStringIcon = {
+      optionText: "",
+      icon: "delete",
+    }
+
+    this.dropdownOptions = [option1,option2];
+
+    this.translate.get("Posts.Option.report").subscribe((res: string) => {
+      this.dropdownOptions[0].optionText = res;
+    })
+    this.translate.get("Posts.Option.delete").subscribe((res: string) => {
+      this.dropdownOptions[1].optionText = res;
+    })
+
+    if (this.post.userId !== this.currentUser.userId) {
+      this.dropdownOptions.splice(this.dropdownOptions.length - 1);
+    }
+  }
+
+  /**
+   * Select Option on burgerMenu
+   * @param option
+   */
+  onOptionSelected(option: OptionStringIcon) {
+    if (option === this.dropdownOptions[0]) {
+      this.test();
+    }
+    else if (option === this.dropdownOptions[1]) {
+      this.removePost(this.post);
+    }
   }
 
   reactPost(post : Post) {
@@ -27,6 +83,8 @@ export class CardPostComponent {
     this.delete.emit(post);
   }
 
-
+  test() {
+    this.snackbarService.showSnackbar();
+  }
 
 }
--- src/app/shared/component/card-post/card-post.module.ts
+++ src/app/shared/component/card-post/card-post.module.ts
@@ -3,6 +3,10 @@ import {NgModule} from '@angular/core';
 import {CardPostComponent} from "./card-post.component";
 import {LikeButtonModule} from "../button-component/like-button/like-button.module";
 import {DeleteButtonModule} from "../button-component/delete-button/delete-button.module";
+import {DropdownMenuModule} from "../dropdown-menu/dropdown-menu.module";
+import {MatDialogModule} from "@angular/material/dialog";
+import {TranslateModule} from "@ngx-translate/core";
+import {CapitalizeFirstLetterPipeModule} from "../../pipes/capitalize-first-lettre.module";
 
 
 @NgModule({
@@ -12,7 +16,11 @@ import {DeleteButtonModule} from "../button-component/delete-button/delete-butto
   imports: [
     CommonModule,
     LikeButtonModule,
-    DeleteButtonModule
+    DeleteButtonModule,
+    DropdownMenuModule,
+    MatDialogModule,
+    TranslateModule,
+    CapitalizeFirstLetterPipeModule
   ],
   exports: [CardPostComponent]
 })
